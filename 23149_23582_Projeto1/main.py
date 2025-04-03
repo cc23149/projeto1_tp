109a0a372c4c12512ebb4f6eeba0e338a5694cd7
cc23582 <cc23582@g.unicamp.br>
2025-04-03 13:52:26 -0300

    commit

--- 23149_23582_Projeto1/main.py
+++ 23149_23582_Projeto1/main.py
@@ -1,11 +1,11 @@
-import som, prod, os, pri
+import som, prod, os
 
 def seletorDeOpcoes():
     opcao = -1
     while opcao != 0:
         os.system('cls') or None
         print("Seletor de Opções - Projeto 1")
-        print("=========================//=========================")
+        print("============= // =============\n")
         print("0 - Terminar Programa")
         print("1 - Primos")
         print("2 - Raiz Quadrada")
@@ -16,40 +16,56 @@ def seletorDeOpcoes():
             case 1:
                 primos()
             case 2:
-                raizQuadrada()
+                fazRaizQuadrada()
             case 3:
                 numerosDeFibonacci()
             case 4:
                 processamentoDeDados()
-            case 0:
-                print("Finalizando...")
-                break
-        input("Digite [Enter] para prosseguir...")
 
+        if opcao != 0:
+            input("Tecle [Enter] para retornar ao seletor de opções: ")
 
 def primos():
-    print("1. Números primos num intervalo")
-    numInicial = int(input("Informe o valor inicial do intervalo(valor > 2): "))
-    while numInicial <= 2:
-        print("O valor inicial precisa ser maior que 2!")
-        numInicial = int(input("Informe o valor inicial do intervalo: "))
-    numFim = int(input("Informe o valor final do intervalo : ")) 
+    print("\n1. Números primos numero intervalo\n")
+
+    valor_inicial = -1
+    while valor_inicial <= 2:
+        valor_inicial = int(input("Informe o valor inicial do intervalo: "))
+        if valor_inicial <= 2:
+            print("O valor inicial precisa ser maior que 2!")
+
+    valor_final = int(input("Informe o valor final do intervalo : ")) 
     soma = som.Somatoria()
-    primo = pri.Primo()
 
-    print(f"Os primos entre {numInicial} e {numFim} são: ")
+    print(f"Os primos nesse intealo são: ")
+    for numero in range(valor_inicial, valor_final + 1, 1):
 
-    for i in range(numInicial, numFim + 1):  #Enquanto esta dentro do perido
-        if primo.ehPrimo(numInicial):
-            print(numInicial, end=", ")
-            soma.somar(numInicial)  #se o número não tiver nenhum divisor somanmos ele aos primos
+        qts_divisores = 0
+        metade = numero // 2
+        for possivel_divisor in range(2, metade + 1, 1):
+            if numero % possivel_divisor == 0:
+                qts_divisores += 1
 
-        numInicial += 1        
+            possivel_divisor += 1
 
-    print(f"\nA soma dos primos é: {soma.valor}")
-    print(f"A média aritmética dos primos é: {soma.media_aritmetica()}")
+        if qts_divisores == 0:
+            if numero > valor_final:
+                print( numero,end=", ")
+                soma.somar(numero)
+            else:
+                print(numero)
+                soma.somar(numero)
+        
+        numero += 1
 
-def raizQuadrada():
+    print(f"\nA soma desses primos é: {soma.valor}")
+    try:
+        print(f"A média aritmética dos primos é: {soma.media_aritmetica():5.2f}")
+    except Exception as erro:
+        print(erro)
+
+
+def fazRaizQuadrada():
     pass
 
 def numerosDeFibonacci():
